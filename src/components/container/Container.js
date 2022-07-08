2998b6e6c6605c98d003982f874d52d1d99d6099
Nataliia Lyfar <lifarnatala@gmail.com>
2022-07-08 14:57:18 +0300

    return prev style

--- src/components/container/Container.js
+++ src/components/container/Container.js
@@ -3,6 +3,5 @@ import styled from 'styled-components';
 export const Container = styled.div`
 display: grid;
 grid-template-columns: 1fr;
-grid-gap: ${p => p.theme.space[3]}px;
 padding-bottom: ${p => p.theme.space[5]}px;
 `;
